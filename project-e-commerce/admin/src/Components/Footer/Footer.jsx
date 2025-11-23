326db7be9a9248f403460202e5879f8e43c76983
Mahesh <maheshwaghmare7744@gmail.com>
2025-11-23 16:05:10 +0530

    inital commit

---
+++ project-e-commerce/admin/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>contact- 7020853631 </p>
+      <p>Copyright @ 2025 - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
